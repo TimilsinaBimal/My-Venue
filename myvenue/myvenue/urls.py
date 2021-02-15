a4e874556bdee2318676562866dea1c988a244e3
TimilsinaBimal <timilsina.bml05@gmail.com>
2021-02-15 16:05:09 +0545

    [MODIFIED] Admin Panel Titles

--- myvenue/myvenue/urls.py
+++ myvenue/myvenue/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,11 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('tinymce/', include('tinymce.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATICFILES_DIRS)
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = 'My Venue Administration'
+admin.site.site_title = "My Venue  Administration"
+admin.site.index_title = "My Venue  Administration"
